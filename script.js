539544198def8dc26c2650b4d7ef610e98d08fb0
Dishita Karkare <123790338+haza158@users.noreply.github.com>
2025-11-30 09:29:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function searchArticles() {
+    let input = document.getElementById("searchBar").value.toLowerCase();
+    let cards = document.querySelectorAll(".article-card");
+
+    cards.forEach(card => {
+        let title = card.dataset.title.toLowerCase();
+        let desc = card.dataset.desc.toLowerCase();
+
+        if (title.includes(input) || desc.includes(input)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+}
